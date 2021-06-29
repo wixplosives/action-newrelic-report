baeb33dda840ac1b631a5bf1f1001aa29ee4da32
Yarden Porat <yardenporat@gmail.com>
2021-06-29 13:08:11 +0300

    rebuild changes

--- dist/index.js
+++ dist/index.js
@@ -252,16 +252,16 @@ function loadLocalMetricsFromFile(filePath) {
             const rawMetrics = JSON.parse(fileContent);
             if (rawMetrics) {
                 metrics['bundle_time_duration'] = {
-                    avg: rawMetrics.bundleTime,
+                    regularAvg: rawMetrics.bundleTime,
                     normalizedAvg: rawMetrics.bundleTime,
                     normalizedObs: 1
                 };
-                for (const k in rawMetrics.avg) {
+                for (const k in rawMetrics.regularAvg) {
                     const newRelicKeyName = k.replace(/ /g, '_');
                     metrics[newRelicKeyName] = {
-                        avg: rawMetrics.avg[k],
-                        normalizedAvg: rawMetrics.normalizedAvg[k]['duration'],
-                        normalizedObs: rawMetrics.normalizedAvg[k]['observations']
+                        regularAvg: rawMetrics.regularAvg[k],
+                        normalizedAvg: rawMetrics.avg[k]['duration'],
+                        normalizedObs: rawMetrics.avg[k]['observations']
                     };
                 }
             }
@@ -302,7 +302,7 @@ function calcChangeForMetrics(metrics, nrValues) {
     const changes = {};
     for (const k in metrics) {
         changes[k] = {
-            avg: Math.round((metrics[k]['avg'] / nrValues[k] - 1) * 100),
+            regularAvg: Math.round((metrics[k]['regularAvg'] / nrValues[k] - 1) * 100),
             normalizedAvg: Math.round((metrics[k]['normalizedAvg'] / nrValues[k] - 1) * 100)
         };
     }
@@ -312,12 +312,12 @@ exports.calcChangeForMetrics = calcChangeForMetrics;
 function makeMDReportStringForMetrics(localMetrics, newrelicLatest, os) {
     const changes = calcChangeForMetrics(localMetrics, newrelicLatest);
     const reportRows = new Array('');
-    reportRows.push(`| Test (${os}) | Avg (ms) | Normalized Avg (ms) / Obs (n)| Average From NewRelic (ms)| Change % (Avg)|Change % (Normalized Avg)|`);
+    reportRows.push(`| Test (${os}) | Regular Avg (ms) | Normalized Avg (ms) / Obs (n)| Average From NewRelic (ms)| Change % (Avg)|Change % (Normalized Avg)|`);
     reportRows.push('|----|---:|---:|---:|---:|---:|');
     for (const k in localMetrics) {
         const roundedNormalizedAvg = Math.round(localMetrics[k]['normalizedAvg']);
-        const roundedAvg = Math.round(localMetrics[k]['avg']);
-        reportRows.push(`|${k}|${roundedAvg}|${roundedNormalizedAvg} / ${localMetrics[k]['normalizedObs']} |${newrelicLatest[k]}|${changes[k]['avg']}%|${changes[k]['normalizedAvg']}%|`);
+        const roundedAvg = Math.round(localMetrics[k]['regularAvg']);
+        reportRows.push(`|${k}|${roundedAvg}|${roundedNormalizedAvg} / ${localMetrics[k]['normalizedObs']} |${newrelicLatest[k]}|${changes[k]['regularAvg']}%|${changes[k]['normalizedAvg']}%|`);
     }
     return reportRows.join('\n');
 }
